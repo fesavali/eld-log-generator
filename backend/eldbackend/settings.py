9b2f178258d178c770ad7a2c37d414fa4dd5e816
fesavali <felixnzioki99@gmail.com>
2025-09-14 22:14:35 +0200

    update cors rules

--- backend/eldbackend/settings.py
+++ backend/eldbackend/settings.py
@@ -87,15 +87,41 @@ REST_FRAMEWORK = {
 
 
 # CORS settings
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https:\/\/eld-log-generator-\w+\.vercel\.app$",
+    r"^https:\/\/eld-log-generator-\w+-felix-savalis-projects\.vercel\.app$",
+]
+
+# include specific domains
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://eld-log-generator-production.up.railway.app",
-    "https://eld-log-generator-pa1h91y8q-felix-savalis-projects.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://eld-log-generator-production.up.railway.app",
-    "https://eld-log-generator-pa1h91y8q-felix-savalis-projects.vercel.app",
+    "https://*.vercel.app",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 STATIC_URL = '/static/'
